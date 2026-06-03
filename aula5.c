58fe384ba0ffaf657f310baea15458e16b21f70e
“Francislaino” <franx.barbosa@gmail.com>
2026-06-02 21:26:48 -0300

    treino de switch, aula 4

--- aula5.c
+++ aula5.c
@@ -3,7 +3,9 @@
 
 int main() {
 
-    char nome[20];
+
+
+    /*char nome[20];
     float nota1, nota2, nota3, nota4, media;
 
     printf("digite o nome do aluno:\n");
@@ -21,10 +23,44 @@ int main() {
     media = (nota1 + nota2 + nota3 + nota4) / 4;
 
     if (media >= 7) {
-        printf("Sua media anual é:%.2f, parabens %s, voce foi aprovado.\n", media, nome);
+        printf("Sua media anual e:%.2f, parabens %s, voce foi aprovado.\n", media, nome);
     } else {
-        printf("sua media anual é: %.2f, que pena %s, voce foi reprovado;\n",media, nome);
+        printf("sua media anual e: %.2f, que pena %s, voce foi reprovado;\n",media, nome);
+    }*/
+
+    int opcao;
+
+    printf("Digite um numero de 1 a 7:\n");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+    case 1:
+        printf("Domingo.\n");
+        break;
+    case 2:
+        printf("segunda\n");
+        break;
+    case 3:
+        printf("Terca\n");
+        break;
+    case 4:
+        printf("Quarta\n");
+        break;
+    case 5:
+        printf("Quinta\n");
+        break;
+    case 6:
+        printf("Sexta.\n");
+        break;
+    case 7:
+        printf("Sabado.z\n");
+        break;
+    default:
+        printf("digite um numero de 1 a 7.\n");
+        break;
     }
+
+
 
 
 
