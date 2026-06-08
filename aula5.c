b87f0f876cb212ba77bbae6bee7023a1a0a913de
“Francislaino” <franx.barbosa@gmail.com>
2026-06-08 10:35:54 -0300

    praticando switch, aula 4

--- aula5.c
+++ aula5.c
@@ -28,38 +28,56 @@ int main() {
         printf("sua media anual e: %.2f, que pena %s, voce foi reprovado;\n",media, nome);
     }*/
 
-    int opcao;
+   float num1;
+   float num2;
+   int opcao;
+   float resultado;
 
-    printf("Digite um numero de 1 a 7:\n");
+    printf("====CALCULADORA====\n");
+    printf("digite o primeiro numero:\n");
+    scanf("%f", &num1);
+    printf("digite o segundo numero.\n");
+    scanf("%f", &num2);
+
+    printf("\nEscolha qual operaçao deseja realizar:\n");
+    printf("1. adição\n");
+    printf("2. subtração\n");
+    printf("3. multiplicação \n");
+    printf("4. divisão \n");
+
     scanf("%d", &opcao);
 
-    switch (opcao){
+    switch (opcao)
+    {
     case 1:
-        printf("Domingo.\n");
+         resultado = num1 + num2;
+
+         printf("a soma de %.2f mais %.2f é igual a %.2f.\n", num1, num2, resultado);
         break;
     case 2:
-        printf("segunda\n");
+         resultado = num1 - num2;
+
+         printf("a subtracao de %.2f menos %.2f é igual a %.2f.\n", num1, num2, resultado);
         break;
     case 3:
-        printf("Terca\n");
+         resultado = num1 * num2;
+
+         printf("a multiplicação de %.2f vezes %.2f é igual a %.2f.\n", num1, num2, resultado);
         break;
     case 4:
-        printf("Quarta\n");
+         resultado = num1 / num2;
+
+         printf("a divisão de %.2f divido por %.2f é igual a %.2f.\n", num1, num2, resultado);
         break;
-    case 5:
-        printf("Quinta\n");
-        break;
-    case 6:
-        printf("Sexta.\n");
-        break;
-    case 7:
-        printf("Sabado.z\n");
-        break;
+    
     default:
-        printf("digite um numero de 1 a 7.\n");
+        printf("numero invalido. digite um numero de 1 a 4");
         break;
     }
 
+
+
+    
 
 
 
