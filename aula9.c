70584db5e68f4777e017175231bbc4e0b3ffc209
“Francislaino” <franx.barbosa@gmail.com>
2026-07-10 09:39:32 -0300

    aula 9, matrizes

---
+++ aula9.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h> 
+
+int main() {
+
+
+    /*int mat[3][3];
+
+    mat[0][0] = 1;
+    mat[0][1] = 2;
+    mat[0][2] = 3;
+
+    mat[1][0] = 4;
+    mat[1][1] = 5;
+    mat[1][2] = 6;
+
+    mat[2][0] = 7;
+    mat[2][1] = 8;
+    mat[2][2] = 9;
+
+    printf("imprimindo a primeira linha:\n");
+    printf("%d %d %d\n", mat[0][0], mat[0][1], mat[0][2]);
+
+    printf("imprimindo a segunda linha:\n");
+    printf("%d %d %d\n", mat[1][0], mat[1][1], mat[1][2]);
+
+    printf("imprimindo a terceira linha:\n");
+    printf("%d %d %d\n", mat[2][0], mat[2][1], mat[2][2]);*/
+
+    int mat[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    int i, j;
+
+    printf("imprimindo a primeira linha:\n");
+    for(j=0;j<3;j++){
+        printf("%d ", mat[0][j]);
+    }
+
+    printf("\nimprimindo a matriz completa:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    } 
+
+    return 0;
+}
